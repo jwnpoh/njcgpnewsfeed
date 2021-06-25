fd6987c74dc8115f9d49ee929527fa776a21c29a
jwnpoh <jwn.poh@gmail.com>
2021-06-25 23:52:18 +0800

    rm oauth and use gcp service account instead

--- db/sheets.go
+++ db/sheets.go
@@ -2,43 +2,18 @@
 package db
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
 	"os"
 
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
 
-// Retrieve a token from env then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-    tokenString := os.Getenv("TOKEN")
-    r := bytes.NewReader([]byte(tokenString))
-
-	tok := &oauth2.Token{}
-    err := json.NewDecoder(r).Decode(tok)
-    if err != nil {
-        log.Fatalf("unable to decode token - %v", err)
-    }
-
-	return config.Client(context.Background(), tok)
-}
-
 func newSheetsService(ctx context.Context) (*sheets.Service, error) {
 	b := os.Getenv("CREDENTIALS")
-	config, err := google.ConfigFromJSON([]byte(b), "https://www.googleapis.com/auth/spreadsheets")
-	if err != nil {
-		return nil, fmt.Errorf("unable to parse client secret file to config: %v", err)
-	}
-	client := getClient(config)
 
-	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
+	srv, err := sheets.NewService(ctx, option.WithCredentialsJSON([]byte(b)))
 	if err != nil {
 		return srv, fmt.Errorf("unable to retrieve Sheets client: %v", err)
 	}
@@ -67,4 +42,3 @@ func getSheetData(srv *sheets.Service, sheetRange string) (*sheetData, error) {
 
 	return &sd, nil
 }
-
